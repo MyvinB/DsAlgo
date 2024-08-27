bfc5f5e19d51512534b44acaf1b20ba0e5f44d15
myvinB <myvinbarboza@gmail.com>
2024-08-27 13:41:24 +0530

    LC1514

--- src/Daily/LC1514.java
+++ src/Daily/LC1514.java
@@ -1,9 +1,6 @@
 package Daily;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class LC1514 {
 
@@ -14,40 +11,39 @@ public class LC1514 {
     }
 
     public static double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        PriorityQueue<double[]> pq = new PriorityQueue<>((a,b)->{
-            if(a[0]==b[0]) return Double.compare(a[1],b[1]);
-            else return Double.compare(a[0],b[0]);
-        });
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a,b)->Double.compare(b[0],a[0]));
         //Now make adjList
-        List<List<double[]>> adjList = new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adjList.add(new ArrayList<>());
-        }
+        Map<Integer,List<double[]>> adjList = new HashMap<>();
         for(int i=0;i<edges.length;i++){
             int from = edges[i][0];
             int to = edges[i][1];
             double distance = succProb[i];
-            adjList.get(from).add(new double[]{distance,to});
+            adjList.computeIfAbsent(from,k->new ArrayList<>()).add(new double[]{distance,to});
+            adjList.computeIfAbsent(to,k->new ArrayList<>()).add(new double[]{distance,from});
         }
         double[] dist = new double[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        pq.offer(new double[]{1,0});
+        Arrays.fill(dist,Integer.MIN_VALUE);
+        pq.offer(new double[]{1,start_node});
+        dist[start_node] = 1d;
 
         while(!pq.isEmpty()){
             double[] node = pq.poll();
             double d = node[0];
             int u = (int) node[1];
-            for(double[] v:adjList.get(u)){
+            if(u==end_node){
+                return d;
+            }
+            for(double[] v:adjList.getOrDefault(u,new ArrayList<>())){
                 double totalDistance = d *v[0];
                 int k = (int) v[1];
-                if(totalDistance<dist[k]){
+                if(totalDistance >dist[k]){
                     dist[k] = totalDistance;
                     pq.offer(new double[]{totalDistance,k});
                 }
             }
         }
 
-        return dist[n-1];
+        return dist[end_node]!=Integer.MIN_VALUE?dist[end_node]:0;
 
     }
 }
