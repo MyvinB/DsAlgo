de06db83bc5ae031a7d96bae8a5672812d5d41f5
MyvinB <myvinbarboza@gmail.com>
2025-04-23 07:56:58 +0530

    Easy today

---
+++ src/Daily/LC1399.java
@@ -0,0 +1,39 @@
+package Daily;
+
+import javax.swing.*;
+import java.util.HashMap;
+
+/**
+ * Created by myvinbarboza on 23/04/25 at 7:41 AM
+ * Today is a Wednesday
+ */
+public class LC1399 {
+
+    public static void main(String[] args) {
+        System.out.println(countLargestGroup(13));
+    }
+
+    public static int countLargestGroup(int n) {
+        int[] count = new int[37];
+        int size = 0;
+        for(int i=1;i<=n;i++){
+            int sum = 0;
+            int a = i;
+            while(a!=0){
+                sum+=a%10;
+                a=a/10;
+            }
+            count[sum]++;
+            size = Math.max(size,count[sum]);
+        }
+        int total=0;
+        for(int i=1;i<37;i++){
+            if(count[i]==size){
+                total+=1;
+            }
+        }
+        return total;
+    }
+
+
+}
